c08053d4d0019436359cd1e2ac6d05f793d1108d
Ethan-C11 <ethan.collignon.1102@gmail.com>
2025-11-16 21:02:59 +0100

    feat: added typeorm

--- src/models/logsModel.ts
+++ src/models/logsModel.ts
@@ -1,7 +1,15 @@
-export interface logsModel {
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm"
+
+@Entity()
+export class logsModel {
+    @PrimaryGeneratedColumn()
     id: number;
+    @Column()
     microservice: string;
+    @Column()
     microservice_id: number;
+    @Column()
     code_of_event: string;
+    @Column()
     event: string;
 }
