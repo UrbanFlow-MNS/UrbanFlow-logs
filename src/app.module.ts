e6fcab239f8bebd9c96b6225a3616b4dacef7432
Ethan-C11 <ethan.collignon.1102@gmail.com>
2025-11-19 09:08:39 +0100

    feat: configured Typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { logsModel } from './Objects/Models/logsModel';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: Number(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      entities: [logsModel],
+      synchronize: true, // TODO: à désactiver en production
+    }),
+    TypeOrmModule.forFeature([logsModel]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
