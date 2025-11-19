56341554dd4c4e727d5ba5eeadf55bae41d2352b
Ethan-C11 <ethan.collignon.1102@gmail.com>
2025-11-19 14:31:53 +0100

    fix: ; instead of ,

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,7 +10,7 @@ import { ConfigModule } from '@nestjs/config';
   imports: [
     ConfigModule.forRoot(
       {
-        isGlobal : true;
+        isGlobal : true,
       }
     ),
     TypeOrmModule.forRoot({
