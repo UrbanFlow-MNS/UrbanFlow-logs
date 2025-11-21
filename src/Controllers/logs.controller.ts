65fddc71b6dc5cbf93e0ae2a8e679c124d619e34
Ethan-C11 <ethan.collignon.1102@gmail.com>
2025-11-21 15:28:34 +0100

    feat: created post create route

--- src/Controllers/logs.controller.ts
+++ src/Controllers/logs.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { LogsService } from '../Services/logs.service';
 import { LogsEntity } from '../Objects/Entities/logs.entity';
+import { LogsDto } from '../Objects/DTOs/logs.dto';
 
 @Controller('logs')
 export class LogsController {
@@ -10,4 +11,10 @@ export class LogsController {
   async findAll(): Promise<LogsEntity[]> {
     return await this.logsService.getAllLogs();
   }
+
+  @Post()
+  async create(@Body() log: LogsDto): Promise<LogsDto> {
+    return await this.logsService.createLogs(log);
+  }
+
 }
--- src/Services/logs.service.ts
+++ src/Services/logs.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { LogsEntity } from '../Objects/Entities/logs.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { LogsDto } from '../Objects/DTOs/logs.dto';
 
 
 @Injectable()
@@ -13,6 +14,11 @@ export class LogsService {
 
   async getAllLogs(): Promise<LogsEntity[]> {
     return await this.logsRepository.find();
+  }
+
+  async createLogs(logsDto: LogsDto) : Promise<LogsDto> {
+    const log = this.logsRepository.create(logsDto);
+    return await this.logsRepository.save(log);
   }
 
 }
