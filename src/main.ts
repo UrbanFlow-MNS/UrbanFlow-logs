42ca4e25373d6dd286c7290ffef7e06d349e9f2a
Ethan-C11 <ethan.collignon.1102@gmail.com>
2026-04-08 08:39:50 +0200

    fix: tcp

--- src/main.ts
+++ src/main.ts
@@ -26,8 +26,16 @@ async function bootstrap() {
         },
     });
 
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.TCP,
+    options: {
+      host: '0.0.0.0',
+      port: Number(process.env.TCP_PORT) || 4002
+    },
+  });
+
     await app.startAllMicroservices()
-    await app.listen(process.env.PORT ?? 4002);
+    await app.listen(process.env.TCP_PORT ?? 4002);
 }
 
 bootstrap();
